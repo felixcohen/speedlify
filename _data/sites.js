b456bf0d30d537634bae4fec275a43f494fe53f3
Rich Holman <dogwonder@gmail.com>
2020-10-27 14:27:39 +0000

    add new sites

---
+++ _data/sites.js
@@ -0,0 +1,38 @@
+module.exports = {
+	// netlify: {
+	// 	description: "Netlify web sites",
+	// 	options: {
+	// 		freshChrome: "site",
+	// 	},
+	// 	urls: [
+	// 		"https://www.netlify.com/",
+	// 		"https://www.netlify.com/donation-matching/",
+	// 		"https://www.netlify.com/products/build/plugins/",
+	// 		"https://www.netlify.com/with/sitecore/",
+	// 		"https://www.netlify.com/with/drupal/",
+	// 		"https://www.netlify.com/with/wordpress/",
+	// 		"https://www.netlify.com/webinar/a-drupal-journey-to-the-jamstack/",
+	// 		// "https://www.netlify.com/pricing/", // Having Lighthouse issues with LCP on this page
+	// 		"https://jamstackconf.com/",
+	// 		"https://jamstackconf.com/virtual/",
+	// 	]
+	// },
+	wordpress: {
+		description: "Wordpress web sites",
+		options: {
+			freshChrome: "site"
+		},
+		urls: [
+			"https://www.one.org/international/", 
+			"https://www.one.org/us/",
+			"https://www.one.org/africa/", 
+			"https://www.one.org/africa/fr/", 
+			"https://www.one.org/canada/", 
+			"https://www.one.org/canada-fr/", 
+			"https://www.one.org/fr/", 
+			"https://www.one.org/de/", 
+			"https://www.one.org/nl/", 
+			"https://m.one.org/africa/"
+		]
+	}
+};
--- _data/sites/one.js
+++ _data/sites/one.js
@@ -5,8 +5,16 @@ module.exports = {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
 	urls: [
-		"https://www.one.org/international/",
-		"https://www.one.org/us/",
+		"https://one.org/international/", 
+		"https://one.org/us/",
+		"https://one.org/africa/", 
+		"https://one.org/africa/fr/", 
+		"https://one.org/canada/", 
+		"https://one.org/canada-fr/", 
+		"https://one.org/fr/", 
+		"https://one.org/de/", 
+		"https://one.org/nl/", 
+		"https://m.one.org/africa/",
 		"https://act.one.org/sign/debt-relief-2020-can-fr"
 	]
 };
