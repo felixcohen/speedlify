3664afc30cc4dc30d25536f2dda0b99d6ede8dac
Rich Holman <dogwonder@gmail.com>
2020-11-20 11:25:16 +0000

    remove extra urls

--- _data/sites.js
+++ _data/sites.js
@@ -1,22 +1,22 @@
 module.exports = {
-	wordpress: {
-		description: "Wordpress web sites",
-		options: {
-			freshChrome: "site"
-		},
-		urls: [
-			"https://www.one.org/international/", 
-			"https://www.one.org/us/",
-			"https://www.one.org/africa/", 
-			"https://www.one.org/africa/fr/", 
-			"https://www.one.org/canada/", 
-			"https://www.one.org/canada/fr/", 
-			"https://www.one.org/fr/", 
-			"https://www.one.org/de/", 
-			"https://www.one.org/nl/", 
-			"https://m.one.org/africa/"
-		]
-	},
+	// wordpress: {
+	// 	description: "Wordpress web sites",
+	// 	options: {
+	// 		freshChrome: "site"
+	// 	},
+	// 	urls: [
+	// 		"https://www.one.org/international/", 
+	// 		"https://www.one.org/us/",
+	// 		"https://www.one.org/africa/", 
+	// 		"https://www.one.org/africa/fr/", 
+	// 		"https://www.one.org/canada/", 
+	// 		"https://www.one.org/canada/fr/", 
+	// 		"https://www.one.org/fr/", 
+	// 		"https://www.one.org/de/", 
+	// 		"https://www.one.org/nl/", 
+	// 		"https://m.one.org/africa/"
+	// 	]
+	// },
 	mobileoptimisation: {
 		description: "Speed test sites",
 		options: {
